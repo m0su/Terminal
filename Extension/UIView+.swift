cd4999222367fdc391c3455725fb0e95378da603
m0su <sirm0su@gmail.com>
2020-06-16 00:52:50 +0900

    그림자, 깜빡임, 흔들기, 그라데이션 추가

--- Extension/UIView+.swift
+++ Extension/UIView+.swift
@@ -35,6 +35,59 @@ extension UIView {
             self.layer.addSublayer($0)
         }
     }
+
+    func setShadow(color: UIColor) {
+        self.layer.shadowColor = color.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 3)
+        self.layer.shadowRadius = 3
+        self.layer.shadowOpacity = 1.0
+    }
+
+    func makeCircle(borderWidth: CGFloat = 1, borderColor: UIColor) {
+        self.layer.cornerRadius = self.frame.height/2
+        self.layer.borderWidth = borderWidth
+        self.layer.borderColor = borderColor.cgColor
+        self.clipsToBounds = true
+    }
+
+    func startBlink() {
+        UIView.animate(withDuration: 0.7,
+                       delay: 0.0,
+                       options: [.autoreverse, .repeat],
+                       animations: {
+                        self.alpha = 0
+        }, completion: nil)
+    }
+
+    func stopBlink() {
+        alpha = 1
+        layer.removeAllAnimations()
+    }
+
+    func shake() {
+        let animation = CABasicAnimation(keyPath: "position").then {
+            $0.duration = 0.05
+            $0.repeatCount = 3
+            $0.autoreverses = true
+            $0.fromValue = NSValue(cgPoint: CGPoint(x: self.center.x - 5, y: self.center.y))
+            $0.toValue = NSValue(cgPoint: CGPoint(x: self.center.x + 5, y: self.center.y))
+        }
+        self.layer.add(animation, forKey: "position")
+    }
+
+    func setGradientBackground(color: [UIColor], locations: [NSNumber]? = nil) {
+        if color.contains(.clear) {
+            self.layer.sublayers?.removeAll()
+            return
+        }
+        
+        let gradient: CAGradientLayer = CAGradientLayer().then {
+            $0.frame = self.bounds
+            $0.colors = color.map { $0.cgColor }
+            $0.locations = locations
+            self.layer.insertSublayer($0, at: 0)
+        }
+    }
 }
 // 호출
 let view = UIView()
